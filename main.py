3fce194aedb3aafaae59c9102989ee23b3dad164
ghts2110 <gabrieltorres2110@gmail.com>
2025-07-07 03:23:51 -0300

    actions

---
+++ main.py
@@ -0,0 +1,16 @@
+from nes_py import NESEnv
+from nes_py.wrappers import JoypadSpace
+import numpy as np
+import time
+
+from utils.actions import(
+    rom,
+    actions,
+)
+
+def main():
+    print("...")
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/actions.py
@@ -0,0 +1,17 @@
+import os
+
+base_path = os.path.dirname(__file__)
+
+rom = base_path + "../roms/super-mario-bros.nes"
+
+actions = [
+    ['NOOP'],            #0
+    ['right'],           #1
+    ['left'],            #2
+    ['down'],            #3
+    ['up'],              #4
+    ['A'],               #5
+    ['B'],               #6
+    ['start'],           #7
+    ['select'],          #8
+]
