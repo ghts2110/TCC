5b2d0cf842b3873f9dad70989fdebcb2d3783ada
ghts2110 <gabrieltorres2110@gmail.com>
2025-08-31 20:00:01 -0300

    street fighter rom

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-import os, sys, time
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.zelda_env import ZeldaEnv
+from nes_py.wrappers import JoypadSpace
+
+from src.learning.population import create_population, evaluate_population
+from src.mario_agent import CustomMarioAgent
+from src.mario_view import CustomMarioView
+from src.mario_env import CustomMarioEnv
+from utils.actions import (
+    rom
+)
+
 
 def main():
-    env = ZeldaEnv() 
-    env.reset()
+    actions = [
+        ['NOOP'], ['right'], ['left'], ['down'], ['up'],
+        ['A'], ['B'], ['start'], ['select']
+    ]
+    
+    action_map = {
+        "noop": 0,          # ['NOOP']
+        "move_right": 1,    # ['right']
+        "move_left": 2,     # ['left']
+        "down": 3,          # ['down']
+        "up": 4,            # ['up']
+        "jump": 5,          # ['A']
+        "attack": 6,        # ['B']
+        "start": 7,         # ['start']
+        "select": 8         # ['select']
+    }
+    
+    env = JoypadSpace(CustomMarioEnv(rom), actions)
 
-    for _ in range(1200):          
-        env.render(mode='human')    
-        obs, r, done, info = env.step(0)  
-        if done:
-            env.reset()
-        time.sleep(1/60)
+    view = CustomMarioView()
+    bdi_agent = CustomMarioAgent(actions)
+
+    population = create_population(size=5, actions=["noop", "jump", "move_right"])
+    results = evaluate_population(population, view, env, bdi_agent, action_map)
+
+    for i, (agent, score) in enumerate(results):
+        print(f"Agente {i} → Score: {score:.2f}")
+
     env.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
